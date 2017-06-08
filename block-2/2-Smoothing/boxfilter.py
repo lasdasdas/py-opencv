ca9acd0f4595cae11d4ae6ace27a7f4f22afbf33
David <david.serret.mayer@gmail.com>
2017-06-08 23:24:55 +0200

    modified:   ../../.gitignore 	modified:   ../../Extra-viewcamera/cam.py 	modified:   ../1-Noise/gaussian.py 	modified:   ../1-Noise/salt_pepper.py 	modified:   ../2-Smoothing/boxfilter.py 	modified:   ../2-Smoothing/gaussian.py 	modified:   ../2-Smoothing/median.py 	modified:   ../2-Smoothing/sharpening.py 	modified:   ../4-Filters/templatesasfilter.py 	modified:   canny.py 	modified:   gradients.py 	modified:   houghlines.py 	modified:   ownhoughs.py 	modified:   ../6-Aliasing/naivedownsample.py 	modified:   ../chess.jpg 	modified:   ../moon.jpg 	modified:   ../park.jpg 	modified:   ../zebra.jpg

--- block-2/2-Smoothing/boxfilter.py
+++ block-2/2-Smoothing/boxfilter.py
@@ -1,1 +1,17 @@
-#TODO box filter
+#box filter --> it doesent work very well
+
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('../chess.jpg', 0)
+
+kernel = 1/36*np.ones((6 , 6))
+dst = cv2.filter2D(img, -1, kernel)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray'),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst,cmap = 'gray'),plt.title('Sharp filter')
+plt.xticks([]), plt.yticks([])
+plt.show()
--- block-2/5-EdgeDetection/ownhoughs.py
+++ block-2/5-EdgeDetection/ownhoughs.py
@@ -1,1 +1,33 @@
 # Manual implementation of the hougs figures
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+from numpy import cos , sin
+import cv2
+
+
+def gridbelong(grid , point):
+    pass
+
+
+d2r= math.pi/180
+
+
+img = cv2.imread('../chess.jpg',0)
+# smoothing
+kernel = np.ones((3,3),np.float32)/9
+img = cv2.filter2D(img,-1,kernel)
+# apply the canny filter
+dst = cv2.Canny(img,0,40)
+angle=np.linspace(-math.pi/2, math.pi/2, num=1000   )
+print(dst.shape)
+col =300*"rbgkm"
+for i in range(dst.shape[0]):
+    print("line ",i )
+    for j in range(200):
+        if dst[i][j] > 150:
+            point=[i, j]
+            b = (point[0]*cos(angle) + point[1]*sin(angle))
+            plt.plot(angle, b, color= col[j])
+# plt.axis([0, 6, 0, 20])
+plt.show()
